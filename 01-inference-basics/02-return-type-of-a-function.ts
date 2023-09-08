f96228b3670e8de381ec015581c35a106c087bc2
Jeff Lowy <jeffreylowy@gmail.com>
2023-09-08 05:55:29 +0000

    Add comments with notes

--- 01-inference-basics/02-return-type-of-a-function.ts
+++ 01-inference-basics/02-return-type-of-a-function.ts
@@ -1,4 +1,11 @@
-// jlowy: found solution at https://www.typescriptlang.org/docs/handbook/utility-types.html#returntypetype
+/**
+ * The pattern here is a really powerful, as it allows us to extract types from
+ * runtime code instead of declaring them manually. Anything we add to to our
+ * function myFunc will then be reflected in the type.
+ * 
+ * Found solution at
+ * https://www.typescriptlang.org/docs/handbook/utility-types.html#returntypetype
+ */
 
 const myFunc = () => {
 	return 12;
@@ -9,6 +16,7 @@ function myFunc2() {
 }
 
 type MyFuncType = typeof myFunc;
+// Return type is a utility type that is globally available in TS.
 type MyFuncReturnType = ReturnType<MyFuncType>;
 
 type MyFuncType2 = typeof myFunc2;
--- 01-inference-basics/03-typeof-keyword-type-level.ts
+++ 01-inference-basics/03-typeof-keyword-type-level.ts
@@ -9,7 +9,9 @@ const makeQuery = (
 	},
 ) => { };
 
+// Useful for extracting types from functions in third party libraries or code
+// that you don't own. Hovering over MakeQueryParameters, you can see that it
+// returns a tuple with "url: string" and the optional "opts?"" object.
 type MakeQueryParameters = Parameters<typeof makeQuery>;
-// Useful for extracting types from functions in third party libraries or code that you don't own
 type MakeQueryParametersFirstArg = Parameters<typeof makeQuery>[0];
 type MakeQueryParametersSecondArg = Parameters<typeof makeQuery>[1];
